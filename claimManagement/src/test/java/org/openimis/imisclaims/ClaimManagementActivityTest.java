2a06f27727de8a561b56cb9721668c0540327170
vaneck237 <vaneckbilongo@gmail.com>
2022-06-10 12:49:54 +0100

    configure app

--- claimManagement/src/test/java/org/openimis/imisclaims/ClaimManagementActivityTest.java
+++ claimManagement/src/test/java/org/openimis/imisclaims/ClaimManagementActivityTest.java
@@ -4,4 +4,84 @@ import junit.framework.TestCase;
 
 public class ClaimManagementActivityTest extends TestCase {
 
+    /**
+     * Creates a new ClaimManagementActivityTest object.
+     */
+    public ClaimManagementActivityTest ()
+    {
+    }
+
+    /**
+     * Creates a new ClaimManagementActivityTest object.
+     *
+     * @param name DOCUMENT ME!
+     */
+    public ClaimManagementActivityTest (String name)
+    {
+        super(name);
+    }
+
+    //~ Methods ------------------------------------------------------------------------------------
+    //-------------//
+    // assertNears //
+    //-------------//
+    public static void assertNears (String msg,
+                                    double a,
+                                    double b)
+    {
+        assertNears(msg, a, b, 1E-5);
+    }
+
+    //-------------//
+    // assertNears //
+    //-------------//
+    public static void assertNears (String msg,
+                                    double a,
+                                    double b,
+                                    double maxDiff)
+    {
+        System.out.println("Comparing " + a + " and " + b);
+        assertTrue(msg, Math.abs(a - b) < maxDiff);
+    }
+
+    //----------------//
+    // checkException //
+    //----------------//
+    public static void checkException (Exception ex)
+    {
+        System.out.println("Got " + ex);
+        assertNotNull(ex.getMessage());
+    }
+
+    //-------//
+    // print //
+    //-------//
+    public static void print (Object o)
+    {
+        System.out.println(o);
+    }
+
+//    public void testDummy ()
+//    {
+//        // Nothing
+//    }
+//
+
+    public void testDummy ()
+    {
+        // Nothing
+    }
+
+    //---------//
+    // runTest //
+    //---------//
+
+    @Override
+    protected void runTest ()
+            throws Throwable
+    {
+        System.out.println("\n---\n" + getName() + ":");
+        super.runTest();
+        System.out.println("+++ End " + toString());
+    }
 }
