9272800f1a6f71f33c156d296cd71f1512d254ea
vaneck237 <vaneckbilongo@gmail.com>
2022-08-29 12:02:32 +0100

    add tests

--- claimManagement/src/test/java/org/openimis/imisclaims/ClaimManagementActivityTest.java
+++ claimManagement/src/test/java/org/openimis/imisclaims/ClaimManagementActivityTest.java
@@ -2,6 +2,14 @@ package org.openimis.imisclaims;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+
 public class ClaimManagementActivityTest extends TestCase {
 
+    @Test
+    public void addition_isCorrect() throws Exception {
+        int expected = 5;
+        int actual = (2 + 3); // pretend this is a static method in the Util class in your android project
+        assertEquals(expected, actual);
+    }
 }
