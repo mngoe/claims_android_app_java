497f3a2454410c379f660aeec8a1ecd176b7af03
vaneck237 <vaneckbilongo@gmail.com>
2024-12-18 11:51:46 +0100

    fix fetch cheque number

--- claimManagement/src/main/java/org/openimis/imisclaims/network/request/ChequeImportLineGraphQLRequest.java
+++ claimManagement/src/main/java/org/openimis/imisclaims/network/request/ChequeImportLineGraphQLRequest.java
@@ -10,11 +10,9 @@ import java.util.List;
 
 public class ChequeImportLineGraphQLRequest extends BaseGraphQLRequest {
 
-    private static final String URI = "https://csureport.minsante.cm/api/graphql";
-
     @NonNull
     @WorkerThread
     public List<ChequeImportLineQuery.Edge> get(@NonNull String code) throws Exception {
-        return makeSynchronous(new ChequeImportLineQuery(Input.fromNullable(code)), URI).getData().chequeimportline().edges();
+        return makeSynchronous(new ChequeImportLineQuery(Input.fromNullable(code))).getData().chequeimportline().edges();
     }
 }
