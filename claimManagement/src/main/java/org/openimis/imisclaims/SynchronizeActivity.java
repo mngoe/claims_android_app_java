1def822e4853f28b3518eff1766169327099ed79
vaneck237 <vaneckbilongo@gmail.com>
2026-06-30 12:11:53 +0100

    fetch modules configurations

--- claimManagement/src/main/java/org/openimis/imisclaims/SynchronizeActivity.java
+++ claimManagement/src/main/java/org/openimis/imisclaims/SynchronizeActivity.java
@@ -33,6 +33,7 @@ import org.openimis.imisclaims.domain.entity.DiagnosesServicesMedications;
 import org.openimis.imisclaims.domain.entity.Diagnosis;
 import org.openimis.imisclaims.domain.entity.HealthFacility;
 import org.openimis.imisclaims.domain.entity.Medication;
+import org.openimis.imisclaims.domain.entity.ModuleConfig;
 import org.openimis.imisclaims.domain.entity.PaymentList;
 import org.openimis.imisclaims.domain.entity.Program;
 import org.openimis.imisclaims.domain.entity.Service;
@@ -41,6 +42,7 @@ import org.openimis.imisclaims.network.exception.HttpException;
 import org.openimis.imisclaims.tools.Log;
 import org.openimis.imisclaims.tools.StorageManager;
 import org.openimis.imisclaims.usecase.FetchClaimAdmins;
+import org.openimis.imisclaims.usecase.FetchConfigs;
 import org.openimis.imisclaims.usecase.FetchControls;
 import org.openimis.imisclaims.usecase.FetchDiagnosesServicesItems;
 import org.openimis.imisclaims.usecase.FetchDiagnosis;
@@ -121,7 +123,7 @@ public class SynchronizeActivity extends ImisActivity {
 
         importMasterData.setOnClickListener(view -> requestPickDatabase());
         downloadMasterData.setOnClickListener(view -> DownloadMasterData()); //TODO Not yet implemented
-        downloadMasterData.setVisibility(View.GONE);
+        downloadMasterData.setVisibility(View.VISIBLE);
         checkUpdate.setOnClickListener(view -> CheckUpdate());
 
     }
@@ -265,17 +267,7 @@ public class SynchronizeActivity extends ImisActivity {
 
     public void DownloadMasterData() {
         if (global.isNetworkAvailable()) {
-            String progress_message = getResources().getString(R.string.application);
-            pd = ProgressDialog.show(this, getResources().getString(R.string.initializing), progress_message);
-            Thread thread = new Thread(() -> {
-                if(downloadControls()){
-                    runOnUiThread(() -> {
-                        pd.dismiss();
-                        downloadAdmins();
-                    });
-                }
-            });
-            thread.start();
+            downloadControls();
         } else {
             ErrorDialogBox(getResources().getString(R.string.CheckInternet));
         }
@@ -396,6 +388,7 @@ public class SynchronizeActivity extends ImisActivity {
                         runOnUiThread(() -> {
                             pd.dismiss();
                             Toast.makeText(SynchronizeActivity.this, getResources().getString(R.string.installed_updates), Toast.LENGTH_LONG).show();
+                            downloadConfigs();
                         });
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -545,6 +538,7 @@ public class SynchronizeActivity extends ImisActivity {
 
                         runOnUiThread(() -> {
                             pd.dismiss();
+                            downloadAdmins();
                         });
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -592,7 +586,14 @@ public class SynchronizeActivity extends ImisActivity {
                 } catch (Exception e) {
                     e.printStackTrace();
                     Sentry.captureException(e);
-                    runOnUiThread(() -> pd.dismiss());
+                    runOnUiThread(() -> {
+                        pd.dismiss();
+                        if(!global.isNetworkAvailable()){
+                            Toast.makeText(SynchronizeActivity.this,  getResources().getString(R.string.CheckConnection), Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(SynchronizeActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
                 }
             });
             thread.start();
@@ -716,6 +717,45 @@ public class SynchronizeActivity extends ImisActivity {
             Toast.makeText(this, getResources().getString(R.string.downloadUpdateFail), Toast.LENGTH_SHORT).show();
             Log.e("DownloadUpdate", "Erreur: ", e);
             Sentry.captureException(e);
+        }
+    }
+
+    public void downloadConfigs (){
+        if (global.isNetworkAvailable()){
+            String progress_message = getResources().getString(R.string.getConfig) + "...";
+            progressDialog = ProgressDialog.show(this, getResources().getString(R.string.download), progress_message);
+            Thread thread = new Thread() {
+                public void run() {
+                    try {
+                        List<ModuleConfig> configs = new FetchConfigs().execute();
+                        if(!configs.isEmpty()){
+                            sqlHandler.ClearAll("tblConfig");
+                            for (int i=0; i< configs.size(); i++) {
+                                sqlHandler.InsertConfig(i+1,configs.get(i).getModule(), configs.get(i).getConfig());
+                            }
+                        }
+                        runOnUiThread(() -> {
+                            progressDialog.dismiss();
+                            Toast.makeText(SynchronizeActivity.this, getResources().getString(R.string.downloaded_config), Toast.LENGTH_LONG).show();
+                        });
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Sentry.captureException(e);
+                        runOnUiThread(() -> {
+                            progressDialog.dismiss();
+                            if(!global.isNetworkAvailable()){
+                                Toast.makeText(SynchronizeActivity.this,  getResources().getString(R.string.CheckConnection), Toast.LENGTH_LONG).show();
+                            }else {
+                                Toast.makeText(SynchronizeActivity.this, e.getMessage() + "-" + getResources().getString(R.string.AccessDenied), Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                };
+            };
+            thread.start();
+        } else {
+            runOnUiThread(() -> progressDialog.dismiss());
+            ErrorDialogBox(getResources().getString(R.string.CheckInternet));
         }
     }
 }
